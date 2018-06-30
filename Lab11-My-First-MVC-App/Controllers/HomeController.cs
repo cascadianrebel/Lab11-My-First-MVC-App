d89f991c226a3994cb636744ddc693acc568af1d
Anthony Green <Anthonydgreen90@gmail.com>
2018-06-29 19:57:53 -0700

    added final comments

--- Lab11-My-First-MVC-App/Controllers/HomeController.cs
+++ Lab11-My-First-MVC-App/Controllers/HomeController.cs
@@ -9,7 +9,10 @@ namespace Lab11_My_First_MVC_App.Controllers
 {
     public class HomeController : Controller
     {
-        //the initial page view
+        /// <summary>
+        /// initiate page view
+        /// </summary>
+        /// <returns>View Index Page</returns>
         [HttpGet]
         public IActionResult Index()
         {
@@ -17,20 +20,31 @@ namespace Lab11_My_First_MVC_App.Controllers
         }
 
         //posts the parameters for the query
+        /// <summary>
+        /// queries database with given parameters
+        /// </summary>
+        /// <param name="begYear">Beginning Year</param>
+        /// <param name="endYear">Ending Year</param>
+        /// <returns>results page data</returns>
         [HttpPost]
         public IActionResult Index(int begYear, int endYear)
         {
             return RedirectToAction("Results", new {startYear = begYear, finalYear = endYear });
         }
 
+        /// <summary>
+        /// Displays the results of the data query
+        /// </summary>
+        /// <param name="startYear">beginning year</param>
+        /// <param name="finalYear">ending year</param>
+        /// <returns></returns>
         public IActionResult Results(int startYear, int finalYear)
         {
             //allows me to send the results data to the view
             ViewData["Message"] = "Person of the year";
 
             Person Person = new Person();
-            //Person.GetPeople(startYear, finalYear);
-            //return View(Person);
+
             return View(Person.GetPeople(startYear, finalYear));
         }
     }
